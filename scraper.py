d26dfa543a59d9a6024b7c8961e8528019c17240
José Sandoval <josesand99v@gmail.com>
2023-09-04 22:18:13 +0000

    encoding utf-8 añadido

--- scraper.py
+++ scraper.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.cooperativa.cl/noticias/pais/michelle-bachelet/bachelet-y-los-50-anos-el-ambiente-politico-esta-toxico/2023-09-04/161631.html'
 
-response = requests.get(url)
+response = requests.get(url, timeout=5)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
     #pretiffy and save the html
-    with open('output.html', 'w') as file:
+    with open('output.html', 'w', encoding='utf-8') as file:
         file.write(soup.prettify())
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
