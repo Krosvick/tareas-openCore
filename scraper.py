6ab73eeef33312e906c307b3baa3bc289f6e6ed5
José Sandoval <josesand99v@gmail.com>
2023-09-04 22:30:08 +0000

    scrapear solo html

--- scraper.py
+++ scraper.py
@@ -7,8 +7,14 @@ response = requests.get(url, timeout=5)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
-    #pretiffy and save the html
+    
+    # Remove <script> tags from the soup
+    for script in soup(['script' , 'style']):
+        script.extract()
+
+    # Save the modified HTML
     with open('output.html', 'w', encoding='utf-8') as file:
         file.write(soup.prettify())
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
